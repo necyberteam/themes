797a3a2a95687ce6fdcc6c98314d9fdf220454c5
LucasNVarella <lnvarella@gmail.com>
2020-06-10 17:34:42 -0400

    Initial Commit

---
+++ CaC19/sites/all/themes/responsive/js/slide.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function() {
+
+/* Slider */	
+	
+ jQuery('.flexslider').flexslider({
+  controlNav: false,
+  directionNav:true,
+  animation: "slide",              //String: Select your animation type, "fade" or "slide"
+  slideshow: true,                //Boolean: Animate slider automatically
+  slideshowSpeed: 7000
+  });
+
+});
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Themes
 
 Repository for all themes across all sites.
+
+Nect-D8 site themes location:
+/var/www/html-drupal8/
+
+CaC19 site themes location:
+/var/www/covid19/
